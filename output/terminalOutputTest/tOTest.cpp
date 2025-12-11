d9ad9febc3dbd94807425211fc2b03c9f441751c
Heston-M <riy21@txstate.edu>
2025-12-11 02:33:03 -0600

    Added simple tester program for terminalOutput

---
+++ output/terminalOutputTest/tOTest.cpp
@@ -0,0 +1,16 @@
+#include "../terminalOutput.h"
+
+int main() {
+    TerminalOutput::printTitle();
+    TerminalOutput::printHeader("Hello, world!");
+    TerminalOutput::printHeader("This is a test message.");
+    TerminalOutput::printHeader("This is a test message with a long message.");
+    TerminalOutput::printHeader("This is a test message with a long message and a long message.");
+    TerminalOutput::printHeader("This is a test message with a long message and a long message and a long message.");
+    TerminalOutput::printHeader("This is a test message with a long message and a long message and a long message and a long message.");
+    TerminalOutput::printHeader("This is a test message with a long message and a long message and a long message and a long message and a long message.");
+    TerminalOutput::printHeader("This is a test message with a long message and a long message and a long message and a long message and a long message and a long message.");
+    TerminalOutput::printHeader("This is a test message with a long message and a long message and a long message and a long message and a long message and a long message and a long message.");
+    TerminalOutput::printHeader("This is a test message with a long message and a long message and a long message and a long message and a long message and a long message and a long message and a long message.");
+    return 0;
+  }
