4e807446e31fdaa88b1958c4b0619e9a1774fcac
Heston-M <riy21@txstate.edu>
2025-12-17 15:01:04 -0600

    Updated terminal output test

--- output/terminalOutputTest/tOTest.cpp
+++ output/terminalOutputTest/tOTest.cpp
@@ -1,16 +1,39 @@
-#include "../terminalOutput.h"
+#include "../../output/output.h"
+#include "../../output/terminalOutput.h"
+#include "../../processes/Process.h"
+#include "../../processes/ReadyQueueList.h"
 
 int main() {
-    TerminalOutput::printTitle();
-    TerminalOutput::printHeader("Hello, world!");
-    TerminalOutput::printHeader("This is a test message.");
-    TerminalOutput::printHeader("This is a test message with a long message.");
-    TerminalOutput::printHeader("This is a test message with a long message and a long message.");
-    TerminalOutput::printHeader("This is a test message with a long message and a long message and a long message.");
-    TerminalOutput::printHeader("This is a test message with a long message and a long message and a long message and a long message.");
-    TerminalOutput::printHeader("This is a test message with a long message and a long message and a long message and a long message and a long message.");
-    TerminalOutput::printHeader("This is a test message with a long message and a long message and a long message and a long message and a long message and a long message.");
-    TerminalOutput::printHeader("This is a test message with a long message and a long message and a long message and a long message and a long message and a long message and a long message.");
-    TerminalOutput::printHeader("This is a test message with a long message and a long message and a long message and a long message and a long message and a long message and a long message and a long message.");
-    return 0;
-  }
+  Output* out = new TerminalOutput();
+
+  out->printTitle();
+  out->printHeader("Hello, world!");
+  out->printHeader("This is a test message.");
+  out->printHeader("This is a test message with a long message.");
+  out->printHeader("This is a test message with a long message and a long message.");
+  out->printHeader("This is a test message with a long message and a long message and a long message.");
+  out->printHeader("This is a test message with a long message and a long message and a long message and a long message.");
+  out->printHeader("This is a test message with a long message and a long message and a long message and a long message and a long message.");
+  out->printHeader("This is a test message with a long message and a long message and a long message and a long message and a long message and a long message.");
+  out->printHeader("This is a test message with a long message and a long message and a long message and a long message and a long message and a long message and a long message.");
+  out->printHeader("This is a test message with a long message and a long message and a long message and a long message and a long message and a long message and a long message and a long message.");
+  
+  out->printMetric(Output::AVG_TURN_TIME, {10.0});
+  out->printMetric(Output::TOTAL_THROUGHPUT, {100.0});
+  out->printMetric(Output::CPU_UTILIZATION, {0.5});
+  out->printMetric(Output::AVG_PROCESSES_IN_Q, {10.0});
+
+  Process* process = new Process(10.0, 10.0);
+  ReadyQueueList* RQList = new ReadyQueueList(1, 0);
+  
+  out->printLiveUpdate(10.0, Output::ARRIVAL_TO_CPU, process, RQList, nullptr);
+  out->printLiveUpdate(10.0, Output::ARRIVAL_TO_RQ, process, RQList, nullptr);
+  out->printLiveUpdate(10.0, Output::DEPARTURE_CPU_IDLE, process, RQList, nullptr);
+  out->printLiveUpdate(10.0, Output::DEPARTURE_NEXT_PROCESS, process, RQList, process);
+  
+  delete process;
+  delete RQList;
+  delete out;
+
+  return 0;
+}
