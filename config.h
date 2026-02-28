c63895b9f006f2c3e7556080ba5d02ca489dba65
Heston-M <hestonmountain@gmail.com>
2026-02-28 15:22:29 -0600

    Updated README and config comments

--- config.h
+++ config.h
@@ -16,9 +16,9 @@ const bool CHOOSE_NUM_CPUS = true;        // Prompt user to choose number of CPU
 const bool CHOOSE_RQ_SETUP = true;        // Prompt user to choose Ready Queue setup at runtime
 const bool CHOOSE_END_CONDITION = true;   // Prompt user to choose end condition at runtime
 
-const int DEFAULT_SCHEDULER = 0;         // Default scheduler if not choosing at runtime (0 = FCFS, 1 = SJF)
+const int DEFAULT_SCHEDULER = 0;         // Default scheduler if not choosing at runtime (0 = FCFS, 1 = SJF, 2 = SRTF)
 const int DEFAULT_NUM_CPUS = 1;           // Default number of CPUs if not choosing at runtime
-const int DEFAULT_RQ_SETUP = 2;           // Default Ready Queue setup if not choosing at runtime (2 = single global RQ)
+const int DEFAULT_RQ_SETUP = 2;           // Default Ready Queue setup if not choosing at runtime (1 = per-CPU RQs, 2 = single global RQ)
 
 const EndCondition DEFAULT_END_CONDITION = PROCESSES_DEPARTED;  // Default end condition if not choosing at runtime (0 = processes arrived, 1 = processes departed, 2 = time limit)
 const int DEFAULT_N = 10000;             // Default number of processes if not choosing at runtime
