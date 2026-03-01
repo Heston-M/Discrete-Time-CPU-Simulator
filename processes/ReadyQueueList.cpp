9714a754ff0464c9f09d787d52fe9e77ba6b49b1
Heston-M <hestonmountain@gmail.com>
2026-02-28 19:05:17 -0600

    Added HRRN logic to RQ logic

--- processes/ReadyQueueList.cpp
+++ processes/ReadyQueueList.cpp
@@ -50,12 +50,12 @@ bool ReadyQueueList::isRQEmpty(int queueIndex) {
   return RQs[queueIndex]->size == 0;
 }
 
-// Insert process into the target Ready Queue based on FCFS or SJF
+// Insert process into the target Ready Queue based on FCFS, SJF, SRTF, or HRRN
 void ReadyQueueList::insertProcessRQ(Process *process, int queueIndex) {
   process->RQindex = queueIndex;
   ReadyQueue *RQ = RQs[queueIndex];
 
-  if (schedulerType == 1 || schedulerType == 2) {      // SJF & SRTM
+  if (schedulerType == 1 || schedulerType == 2) {      // SJF & SRTF
     if (!RQ->head || process->timeLeft < RQ->head->timeLeft) {
       process->next = RQ->head;
       RQ->head = process;
@@ -69,7 +69,7 @@ void ReadyQueueList::insertProcessRQ(Process *process, int queueIndex) {
       p->next = process;
     }
   }
-  else {                                               // FCFS & default
+  else {                                               // FCFS or HRRN or default
     if (RQs[queueIndex]->head == nullptr) {
       RQ->head = process;
       RQ->tail = process;
@@ -86,8 +86,45 @@ Process* ReadyQueueList::dequeueProcessRQ(float clock, int queueIndex) {
   if (isRQEmpty(queueIndex)) {
       throw runtime_error("Error: Attempted to remove process from empty Ready Queue.");
   }
-  Process *p = RQs[queueIndex]->head;
-  RQs[queueIndex]->head = RQs[queueIndex]->head->next;
-  RQs[queueIndex]->size--;
-  return p;
+  if (schedulerType == 3) {                      // HRRN, dequeue process with highest RR
+    float maxRR = 0.0;
+    int maxRRID = -1;
+
+    Process *p = RQs[queueIndex]->head;
+    while (p) {                                  // Find process with highest RR
+      float RR = 1 + (clock - p->arrivalTime) / p->serviceTime;
+      if (RR > maxRR) {
+        maxRR = RR;
+        maxRRID = p->id;
+      }
+      p = p->next;
+    }
+
+    p = RQs[queueIndex]->head;  // Remove process with highest RR from target Ready Queue
+    Process *prev = nullptr;
+    while (p) {
+      if (p->id == maxRRID) {
+        if (prev) {
+          prev->next = p->next;
+        }
+        else {
+          RQs[queueIndex]->head = p->next;
+        }
+        if (p == RQs[queueIndex]->tail) {
+          RQs[queueIndex]->tail = prev;
+        }
+        RQs[queueIndex]->size--;
+        break;
+      }
+      prev = p;
+      p = p->next;
+    }
+    return p;
+  }
+  else {                        // Not HRRN, dequeue next process from target Ready Queue
+    Process *p = RQs[queueIndex]->head;
+    RQs[queueIndex]->head = RQs[queueIndex]->head->next;
+    RQs[queueIndex]->size--;
+    return p;
+  }
 }
